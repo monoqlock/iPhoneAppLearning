b5b481823e2ece4499eb73d95d06f1e3d46ccdc4
morikawa_kaori <kaori.morikawa0507@gmail.com>
2015-01-12 17:32:32 +0900

    #1-7 FirstApp

---
+++ MyFirstApp/MyFirstApp/MyFirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApp
+//
+//  Created by 森川 かおり on 2015/01/12.
+//  Copyright (c) 2015年 森川 かおり. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyFirstApp/MyFirstApp/MyFirstApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyFirstApp
+//
+//  Created by 森川 かおり on 2015/01/12.
+//  Copyright (c) 2015年 森川 かおり. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)goBack:(UIStoryboardSegue *)segue {}
+
+@end
