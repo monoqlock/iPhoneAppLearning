12adf7311facf0e445e1b36ec668934111c973e2
morikawa_kaori <kaori.morikawa0507@gmail.com>
2015-01-12 18:53:10 +0900

    #12 Tab Bar Controller

---
+++ MyThirdApp/MyThirdApp/MyThirdApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyThirdApp
+//
+//  Created by 森川 かおり on 2015/01/12.
+//  Copyright (c) 2015年 森川 かおり. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyThirdApp/MyThirdApp/MyThirdApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyThirdApp
+//
+//  Created by 森川 かおり on 2015/01/12.
+//  Copyright (c) 2015年 森川 かおり. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
